5f8750a5428e9ce44445d27c621a3dd1a7348370
Agnaldo Rogerio <agnaldoLima_2016@outlook.com>
2020-02-02 10:58:36 -0300

    Primeiro Commit

---
+++ ECS-101-DevOps_with_Fargate/RunFirst.sh
@@ -0,0 +1,8 @@
+git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/workshop-commit
+cp -R ./Continuous-Integration-and-Continuous-Delivery-with-Fargate/ECS-101-DevOps_with_Fargate/* ./fargate-commit
+rm -rf Continuous-Integration-and-Continuous-Delivery-with-Fargate
+cd fargate-commit
+rm -rf images
+git add .
+git commit -m “workshop-bash”
+git push
